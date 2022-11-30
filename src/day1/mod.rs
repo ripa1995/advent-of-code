8cc6abfb7e7e22293cf81f796860649c30ab819d
ripa1995 <roberto.ripamonti@ymail.com>
2022-11-30 16:52:55 +0100

    feat: day1 pt2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,9 +1,21 @@
 use std::{fs::File, str::FromStr, io::{self, BufRead}};
 
-pub fn find_most_calories_carried() -> u64 {
+pub fn find_most_calories_carried(elf_num: usize) -> u64 {
     let file = open_file(String::from_str("./inputs/day1/input.txt").expect("Not a string..."));
-        
-    let mut max = 0;
+    
+    let mut calories = get_grouped_elf_calories(file);
+    
+    calories.sort();
+    
+    let start = calories.len()-elf_num;
+    let end = calories.len();
+    let sum = calories[start..end].iter().sum();
+    
+    sum
+}
+
+fn get_grouped_elf_calories(file: File) -> Vec<u64> {
+    let mut calories = Vec::new();
     let mut sum = 0;
     
     io::BufReader::new(file).lines().for_each(|element| {
@@ -11,13 +23,16 @@ pub fn find_most_calories_carried() -> u64 {
             if let Ok(elem_num) = elem.parse::<u64>() {
                 sum += elem_num;
             } else {
-                max = check_max(max, sum);
+                calories.push(sum);
                 sum = 0;
             }
+        } else {
+            //let's push final elf
+            calories.push(sum);
         }
     });
-    max = check_max(max, sum);
-    max
+
+    calories
 }
 
 fn check_max(max: u64, sum: u64) -> u64 {
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ mod day1;
 fn main() {
     println!(
         "The answer to the ultimate question is {}",
-        day1::find_most_calories_carried()
+        day1::find_most_calories_carried(3)
     );
 }
