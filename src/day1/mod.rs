2a5e28c6cda8b7cf96a9199de7f768890327d050
ripa1995 <roberto.ripamonti@ymail.com>
2022-12-02 10:31:19 +0100

    chore: configs

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,23 +1,31 @@
-use std::{fs::File, str::FromStr, io::{self, BufRead}};
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    str::FromStr,
+};
 
-pub fn find_most_calories_carried(elf_num: usize) -> u64 {
-    let file = open_file(String::from_str("./inputs/day1/input.txt").expect("Not a string..."));
-    
+use crate::open_file;
+
+pub fn find_top_n_calories_carried(elf_num: usize) -> u64 {
+    let file = open_file(
+        String::from_str("./inputs/day1/input.txt").expect("Not a string..."),
+    );
+
     let mut calories = get_grouped_elf_calories(file);
-    
+
     calories.sort();
-    
-    let start = calories.len()-elf_num;
+
+    let start = calories.len() - elf_num;
     let end = calories.len();
     let sum = calories[start..end].iter().sum();
-    
+
     sum
 }
 
 fn get_grouped_elf_calories(file: File) -> Vec<u64> {
     let mut calories = Vec::new();
     let mut sum = 0;
-    
+
     io::BufReader::new(file).lines().for_each(|element| {
         if let Ok(elem) = element {
             if let Ok(elem_num) = elem.parse::<u64>() {
@@ -33,17 +41,4 @@ fn get_grouped_elf_calories(file: File) -> Vec<u64> {
     });
 
     calories
-}
-
-fn check_max(max: u64, sum: u64) -> u64 {
-    if max < sum {
-        sum
-    } else {
-        max
-    }
-}
-
-fn open_file(path: String) -> File {
-    File::open(path)
-    .expect("Failed to read the file...")
 }
